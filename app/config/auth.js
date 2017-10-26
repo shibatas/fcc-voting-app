c3f3805e427d14ef54c36d7cc521013d0d53548b
shibatas <shohei51@gmail.com>
2017-10-26 19:57:15 +0200

    changed key codes for heroku deployment

--- app/config/auth.js
+++ app/config/auth.js
@@ -2,8 +2,8 @@
 
 module.exports = {
 	'githubAuth': {
-		'clientID': process.env.GITHUB_KEY,
-		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': process.env.APP_URL + 'auth/github/callback'
+		'clientID': GITHUB_KEY,
+		'clientSecret': GITHUB_SECRET,
+		'callbackURL': APP_URL + 'auth/github/callback'
 	}
 };
